adf30ce2148183ff7a133b605acc015b04944054
jimmy <jimmy@ownic.com>
2023-04-10 13:36:16 +0400

    update data feeding controller mapping

--- tms/src/main/java/com/digitalsettings/tms/controller/TemperatureDataController.java
+++ tms/src/main/java/com/digitalsettings/tms/controller/TemperatureDataController.java
@@ -5,21 +5,24 @@ import com.digitalsettings.tms.service.TemperatureServiceFacade;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/thermostats/{thermostatId}/temperatures")
+@RequestMapping("/api/v1/thermostats/{thermostatId}/temperatures")
 @RequiredArgsConstructor
 @Validated
+@Slf4j
 public class TemperatureDataController {
     private final TemperatureServiceFacade temperatureServiceFacade;
 
     @PostMapping
     public ResponseEntity<Void> registerTemperatureData(@PathVariable @Min(1) Long thermostatId,
                                                         @RequestBody @Valid TemperatureDataCreateRequest request) {
+        log.info("Registering temperature data for thermostat {} with request {}", thermostatId, request);
         temperatureServiceFacade.registerTemperatureData(thermostatId, request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
